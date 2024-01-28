d15c392cce0da89550aac133c109dcde682688a7
ashish28-github <cookie.ashish@gmail.com>
2024-01-28 02:17:50 -0800

    Update serve.py

--- serve.py
+++ serve.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 @app.route('/',methods = ['POST', 'GET'])
 def mp2_2():
     if request.method == 'POST':
-       p = subprocess.Popen(["python", "stress_cpu.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+       p = subprocess.Popen(["python3", "stress_cpu.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
        output, errors = p.communicate()
        return "SUCCESS"
     else:
        return socket.gethostname()
 
 if __name__ == '__main__':
-   app.run()
+   app.run("0.0.0.0","80")
